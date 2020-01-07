0b5afe13a73d0a03bbd6e79432266efdd37e4ed0
Guilherme Vinicius Barbosa <guilhermevbarbosa1999@gmail.com>
2020-01-07 20:37:06 -0300

    Adicionado express e o body-parser

--- api_node/config/config-express.js
+++ api_node/config/config-express.js
@@ -1,7 +1,17 @@
 var express = require('express');
+var consign = require('consign');
+var bodyParser = require('body-parser');
 
 module.exports = function () {
+    // Faz a ligação com o express na variável app
     var app = express();
+
+    // Tipos de requisições aceitas
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+
+    // Insere na variável app as rotas
+    consign().include('routes').into(app);
 
     return app;
 }
--- api_node/routes/pagamentos.js
+++ api_node/routes/pagamentos.js
@@ -1,6 +1,14 @@
 module.exports = function (app) {
+
     app.get('/pagamentos', function (req, res) {
         console.log('Req OK');
         res.send('OK');
     });
+
+    app.post('/pagamentos/pagamento', function (req, res) {
+        var pagamento = req.body;
+        console.log(pagamento);
+        res.send('Ok')
+    });
+
 }
