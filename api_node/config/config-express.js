f76acc69df2eef8b3af0da695ad2b89079790ba3
Guilherme <guilherme.barbosa@fri.to>
2020-01-08 11:16:49 -0300

    Status code, validação

--- api_node/config/config-express.js
+++ api_node/config/config-express.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var consign = require('consign');
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
 
 module.exports = function () {
     // Faz a ligação com o express na variável app
@@ -9,6 +10,9 @@ module.exports = function () {
     // Tipos de requisições aceitas
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
+
+    // Módulo de validação do express
+    app.use(expressValidator());
 
     // Insere na variável app as rotas, deixa disponível para ser utilizada no ambiente todo utilizado pelo express
     consign().include('routes').then('bd_files').into(app);
--- api_node/routes/pagamentos.js
+++ api_node/routes/pagamentos.js
@@ -6,6 +6,19 @@ module.exports = function (app) {
     });
 
     app.post('/pagamentos/pagamento', function (req, res) {
+
+        // Validando os campos, funciona pelo módulo express validator
+        req.assert("forma_de_pagamento", "Forma de pagamento é obrigatório").notEmpty();
+        req.assert("valor", "Valor é obrigatório e deve ser um decimal").notEmpty().isFloat();
+
+        var errors = req.validationErrors();
+
+        if (errors) {
+            console.log("Erros no form");
+            res.status(400).send(errors);
+            return;
+        }
+
         var pagamento = req.body;
         console.log("Entrou pagamento");
 
@@ -16,15 +29,15 @@ module.exports = function (app) {
         var pagamentoDao = new app.bd_files.PagamentosDao(connection);
 
         pagamentoDao.salva(pagamento, function (err, result) {
-
             if (err) {
-                res.send(err);
+                res.status(500).send(err);
             } else {
                 console.log('pagamento criado');
-                res.json(pagamento);
+                res.location('/pagamentos/pagamento/' + result.insertId);
+                res.status(201).json(pagamento);
             }
-
         });
+
     });
 
 }
