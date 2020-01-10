146d640531cddd7bc6bc7152e9ff334e2a34329b
Guilherme <guilherme.barbosa@fri.to>
2020-01-10 14:09:44 -0300

    Logger de morgan para express

--- api_node/config/config-express.js
+++ api_node/config/config-express.js
@@ -2,6 +2,8 @@ var express = require('express');
 var consign = require('consign');
 var bodyParser = require('body-parser');
 var expressValidator = require('express-validator');
+var morgan = require('morgan');
+var logger = require('../services/logger');
 
 module.exports = function () {
     // Faz a ligação com o express na variável app
@@ -13,6 +15,14 @@ module.exports = function () {
 
     // Módulo de validação do express
     app.use(expressValidator());
+    // Logs com o express, intercepta requisições do express
+    app.use(morgan("common", {
+        stream: {
+            write: function (message) {
+                logger.info(message);
+            }
+        }
+    }));
 
     // Insere na variável app as rotas, deixa disponível para ser utilizada no ambiente todo utilizado pelo express
     consign().include('routes').then('bd_files').then('services').into(app);
