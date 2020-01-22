ba7c8310bc38a6c51bb7b4db2fddf9433a5c5022
Guilherme <guilherme.barbosa@fri.to>
2020-01-22 18:01:26 -0300

    bcrypt WIP

--- api_rest_node/models/user.js
+++ api_rest_node/models/user.js
@@ -1,45 +1,42 @@
 const moment = require('moment');
 const conn = require('../infra/conexao');
-const bcrypt = require('bcryptjs');
 
-const salt = bcrypt.genSaltSync(10);
+const bcrypt = require('bcryptjs');
+const saltRounds = 10;
 
 // MÉTODOS
 class Usuario {
     adiciona(userData, res) {
         const data_criacao = moment().format('YYYY-MM-DD HH:MM:SS');
-        const senha = bcrypt.hashSync(userData.senha, salt);
 
-        const userObj = { ...userData, senha, data_criacao };
-        const sql = 'INSERT INTO usuarios SET ?';
+        var senha = userData.senha;
 
-        conn.query(sql, userObj, (err, results) => {
-            if (err) {
-                res.status(400).json(err);
-            } else {
-                res.status(201).json(results);
-            }
-        })
+        bcrypt.hash(senha, saltRounds, function (err, hash) {
+            const userObj = { ...userData, senha: hash, data_criacao };
+            const sql = 'INSERT INTO usuarios SET ?';
+
+            conn.query(sql, userObj, (err, results) => {
+                if (err) {
+                    res.status(400).json(err);
+                } else {
+                    res.status(201).json(results);
+                }
+            })
+        });
+
     }
 
     login(email, pass, res) {
         const sql = 'SELECT * FROM usuarios WHERE email = ?';
 
         conn.query(sql, [email], (err, results) => {
-            console.log(results[0].senha);
-            console.log(bcrypt.hashSync(pass, salt));
 
             const passBd = results[0].senha;
-            const inputPass = bcrypt.hashSync(pass, salt);
-
-            console.log(inputPass == passBd)
 
             if (err) {
                 res.status(404).json(err);
             } else {
-                if (bcrypt.compareSync(inputPass, passBd)) {
-                    res.status(200).json(results);
-                }
+                console.log('ok')
                 res.status(404).json({
                     message: 'Senha incorreta'
                 });
